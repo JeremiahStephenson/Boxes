64d3a51d257ae6e57d519bbdaee23628ca4bae0c
Jeremiah Stephenson <jeremiahstephenson@gmail.com>
2026-09-09 16:57:52 -0600

    Updates version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.jerry.bit.shapes"
         minSdk = 24
         targetSdk = 37
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
